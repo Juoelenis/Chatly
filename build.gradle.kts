c9493eac86dc3ec617e8b2bd1db6791a2442fada
JuoG <116748990+Juoelenis@users.noreply.github.com>
2024-11-02 21:12:17 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+tasks.register("clean", Delete::class){
+    delete(layout.buildDirectory.get())
+}
---
+++ doc/RELEASE_NOTES.md
@@ -0,0 +1,16 @@
+## v0.1.0-build-1
+
+- Demo
+
+## v0.1.0-build-2
+
+- Added transition Animation for People profile. New transition animation is available in
+  androidx.compose.ui since v1.7.0 alpha and navigation-compose 2.8.0 alpha.
+
+## v0.1.0-build-3
+
+- Upgraded
+
+The source code is available on https://github.com/mobiledevpro/Jetpack-Compose-ChatApp-Template
+
+## v0.1.0-build-4
